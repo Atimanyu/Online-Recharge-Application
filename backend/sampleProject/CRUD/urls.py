e3dee6664f2700cf61d5ec01eadc877c01f7af38
Atimanyu <124989468+Atimanyu@users.noreply.github.com>
2024-08-14 14:38:22 +0530

    Delete backend/sampleProject directory

--- backend/sampleProject/CRUD/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserApi),
-    path('user/<int:id>/', views.UserApi),
-    path('admins/', views.AdminApi),
-    path('admin/<str:id>/', views.AdminApi),
-]
